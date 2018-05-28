6f367dabd2742318666d5a85d664862993fbb280
Christopher Kilding <user@fake>
2018-05-28 12:44:29 +0100

    Hide package-private library classes

--- src/main/scala/com.github.loadtest4j.drivers.gatling/Loadtest4jSimulation.scala
+++ src/main/scala/com.github.loadtest4j.drivers.gatling/Loadtest4jSimulation.scala
@@ -1,5 +1,5 @@
 package com.github.loadtest4j.drivers.gatling {
   import io.gatling.core.Predef.Simulation
 
-  class Loadtest4jSimulation() extends Simulation
+  private class Loadtest4jSimulation() extends Simulation
 }
