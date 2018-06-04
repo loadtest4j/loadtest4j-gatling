6d94f34a57e1144321197016867747250648f0f7
Christopher Kilding <user@fake>
2018-06-04 16:56:59 +0100

    Fix Gatling thread shutdown

--- src/main/scala/io/gatling/GatlingFacade.scala
+++ src/main/scala/io/gatling/GatlingFacade.scala
@@ -13,20 +13,20 @@ import io.gatling.core.controller.{Controller, ControllerCommand}
 import io.gatling.core.stats.DataWritersStatsEngine
 import io.gatling.core.stats.writer.RunMessage
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Try}
+import scala.util.Try
 
 class GatlingFacade(implicit configuration: GatlingConfiguration) {
   def runSimulation(simulation: Simulation): RunResult = {
     // start actor system before creating simulation instance, some components might need it (e.g. shutdown hook)
     val system = ActorSystem("GatlingSystem", GatlingConfiguration.loadActorSystemConfiguration())
 
-    val simClass: Class[Simulation] = Class.forName(simulation.getClass.getName).asInstanceOf[Class[Simulation]]
+    val simClass = Class.forName(simulation.getClass.getName).asInstanceOf[Class[Simulation]]
     val selectedSimulationClass: SelectedSimulationClass = Some(simClass)
 
     val selection = Selection(selectedSimulationClass, configuration)
-    // FIXME derive class from passed-in instance, rather than Gatling's preference for the other way round
+    // Derive class from passed-in instance, rather than Gatling's preference for the other way round
     // val simulation = selection.simulationClass.newInstance
     val simulationParams = simulation.params(configuration)
     val runMessage = RunMessage(simulationParams.name, selection.userDefinedSimulationId, selection.defaultSimulationId, nowMillis, selection.description)
@@ -37,13 +37,17 @@ class GatlingFacade(implicit configuration: GatlingConfiguration) {
     val coreComponents = CoreComponents(controller, throttler, statsEngine, exit, configuration)
     val scenarios = simulationParams.scenarios(system, coreComponents)
 
+    System.gc()
+
     val timeout = Int.MaxValue.milliseconds - 10.seconds
-    val whenRunDone: Future[Try[String]] = coreComponents.controller.ask(ControllerCommand.Start(scenarios))(timeout).mapTo[Try[String]]
-    Await.result(whenRunDone, timeout) match {
-      case Failure(t) => throw t
-      case _ =>
-        simulation.executeAfter()
-        RunResult(runMessage.runId, simulationParams.assertions.nonEmpty)
+    try {
+      val whenRunDone = coreComponents.controller.ask(ControllerCommand.Start(scenarios))(timeout).mapTo[Try[String]]
+      Await.result(whenRunDone, timeout)
+      simulation.executeAfter()
+      RunResult(runMessage.runId, simulationParams.assertions.nonEmpty)
+    } finally {
+      val whenTerminated = system.terminate()
+      Await.result(whenTerminated, 2.seconds)
     }
   }
 }
